781c4155ebde7de3746d29b9cade3bb5aacc910d
YaraIhab2 <yaraihab2@gmail.com>
2024-02-19 23:03:49 +0200

    CreateEmployee functions

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,43 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+
+  console.log('inside async delete')
+  try {
+    
+ const {id }= req.body
+console.log(id )
+
+employee = employee.filter(employee => employee.id !== id);
+ 
+} catch (error) {
+    console.error('Error deleting employee:', error);
+   res.status(500).json({ error: 'ERROR: Employee not deleted' });
+   next(error)
+  }
+ 
+  
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  console.log('inside async create')
+  try {
+    
+ const {id , name}= req.body
+console.log(id , name)
+ const isDuplicated = employee.some(empl => empl.id === id);
+ if (isDuplicated) {
+   return res.status(400).json({ error: 'Existing Employee with same ID' });
+ }
+
+ employee.push( {id , name});
+ 
+} catch (error) {
+    console.error('Error creating employee:', error);
+   res.status(500).json({ error: 'ERROR: Employee not created' });
+   next(error)
+  }
+ 
+  
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -1,3 +1,5 @@
+//const { getEmployees } = require("../BE/controllers/employee")
+
 function fetchEmployees() {
   fetch('http://localhost:3000/api/v1/employee')
     .then(response => response.json())
@@ -32,21 +34,118 @@ function fetchEmployees() {
 // TODO
 // add event listener to submit button
 
+let submitButton = document.querySelector('[type="submit"]');
+submitButton.addEventListener("click", createEmployee);
+
+console.log(submitButton)
 // TODO
 // add event listener to delete button
+let deleteButtons = document.getElementsByClassName('btn btn-danger btn-sm');
+for (var i = 0 ; i < deleteButtons.length; i++) {
+deleteButtons[i].addEventListener('click', 
 
+function(e) {
+ 
+  if (e.target==deleteButtons[i]) {
+   
+    var clickedDeleteButton = e.target;
+    deleteEmployee();
+  }
+
+}
+
+);
+};
 // TODO
 function createEmployee (){
+  console.log ('inside create employee function')
   // get data from input field
-  // send data to BE
+  let inputNameElement  = document.getElementById('name');
+  let inputIdElement = document.getElementById('id');
+// send data to BE
+console.log(inputIdElement.value, inputNameElement.value)
+
+ 
+  fetch('http://localhost:3000/api/v1/employee', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+     },
+    body: JSON.stringify (
+      {
+      id : inputIdElement.value,
+      name: inputNameElement.value
+      }
+    ),
+  })
+
+  .then(response => {
+    if (response.ok) {
+      console.log('Employee is created');
   // call fetchEmployees
+  fetchEmployees();
+      
+    }
+    
+    
+else {
+  console.log ('ERROR')
+      throw new Error('FAILURE: Employee not created');
+     
+}
+  })
+  
+  .catch(error => {
+    console.error('Error creating employee:', error);
+  });
+  
+  
+  
+
+  
 }
 
 // TODO
 function deleteEmployee (){
   // get id
+  const row = clickedDeleteButton.closest('tr'); 
+  const idCell = row.querySelector('td:first-child'); 
+  const idToDelete = idCell.textContent
+    
   // send id to BE
+
+  fetch('http://localhost:3000/api/v1/employee', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+     },
+    body: JSON.stringify (
+      {
+      id : idToDelete
+      }
+    ),
+  })
+
+  .then(response => {
+    if (response.ok) {
+      console.log('Employee is deleted');
   // call fetchEmployees
+  fetchEmployees();
+      
+    }
+    
+    
+else {
+  console.log ('ERROR')
+      throw new Error('FAILURE: Employee not deleted');
+     
 }
+  })
+  
+  .catch(error => {
+    console.error('Error deleting employee:', error);
+  });
+  // call fetchEmployees
+};
 
 fetchEmployees()
